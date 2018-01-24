67e7d4cd7bfaeb1ab45af176fe5975cd470361c1
phraust1612 <phraust@kaist.ac.kr>
2018-01-24 13:03:19 +0900

    single patch request

--- client.py
+++ client.py
@@ -4,29 +4,53 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 
-url = "http://52.183.96.146:5000/submit"
-def clientTest (name : str):
-  with open("xaal.tif","rb") as f:
+def clientSubmit (name : str, url : str, output : str):
+  url = "http://52.183.96.146:5000/" + url
+  with open(name,"rb") as f:
     x = f.read()
     x = base64.b64encode (x)
     x = x.decode ('utf-8')
 
   r = requests.post (url, {'image':x})
   j = r.json ()
-  b = j['ratio'].encode ()
-  b = base64.b64decode (b)
+  del (x)
+  del (r)
 
-  X = np.frombuffer (b, dtype=j['dtype'])
-  X = X.reshape ((j['d0'], j['d1'], j['d2']))
+  if url == "patch":
+    ans = int (j['class'])
+    del (j)
+    return ans
 
-  plt.imshow (X)
-  plt.show()
+  elif url == 'wsi':
+    b = j['ratio'].encode ()
+    b = base64.b64decode (b)
+    X = np.frombuffer (b, dtype=j['dtype'])
+    X = X.reshape ((j['d0'], j['d1'], j['d2']))
+    np.save (output, X)
+    ans = X.shape
+
+    del (j)
+    del (b)
+    del (X)
+    return ans
+
+  else:
+    del (j)
+    return -1
 
 def main ():
   parser = argparse.ArgumentParser()
   parser.add_argument ("--image", help="image file path .tif")
-  rags = parser.parse_args ()
-  clientTest (args.image)
+  parser.add_argument ("--size", help="wsi / patch")
+  parser.add_argument ("--output", help="save file path .npy")
+  args = parser.parse_args ()
+  if args.image == None or args.size == None or args.output == None:
+    print ("argument missing")
+    return -1
+  if args.size != "wsi" and args.size != "patch":
+    print ("unknown size")
+    return -1
+  return clientSubmit (args.image, args.size, args.output)
 
 if __name__ == '__main__':
   main ()
--- server.py
+++ server.py
@@ -6,8 +6,8 @@ import base64
 app = Flask (__name__)
 api = Api (app)
 
-class SubmitTiff(Resource):
-  def post(self):
+class DecodeB64Image (Resource):
+  def post (self):
     try:
       parser = reqparse.RequestParser()
       parser.add_argument('image', type=str)
@@ -17,23 +17,40 @@ class SubmitTiff(Resource):
         x = base64.b64decode (x)
         f.write (x)
 
-      I = readTif ("tmp.tif")
-      X = ratioWSI (I, 10)
-      s = base64.b64encode (X)
-      s = s.decode ('utf-8')
+      del (x)
+      del (args)
+      del (parser)
 
-      del (I)
-      del (X)
-
-      return {'ratio' : s,
-          'dtype' : str(I.dtype),
-          'd0' : I.shape[0],
-          'd1' : I.shape[1],
-          'd2' : I.shape[2]}
     except Exception as e:
       return {'error': str(e)}
 
-api.add_resource(SubmitTiff, '/submit')
+class TakeWSI (DecodeB64Image):
+  def post (self):
+    super().post()
+    I = readTif ("tmp.tif")
+    X = ratioWSI (I, 10)
+    s = base64.b64encode (X)
+    s = s.decode ('utf-8')
+    del (I)
+
+    return {'ratio' : s,
+        'dtype' : str(X.dtype),
+        'd0' : X.shape[0],
+        'd1' : X.shape[1],
+        'd2' : X.shape[2]}
+
+class TakePatch (DecodeB64Image):
+  def post (self):
+    super().post()
+    I = readTif ("tmp.tif")
+    X = classifyPatch (I, 10)
+    X = str (X)
+    del (I)
+
+    return {'class' : X}
+
+api.add_resource (TakeWSI, '/wsi')
+api.add_resource (TakePatch, '/patch')
 
 if __name__ == '__main__':
   app.run(host='52.183.96.146',debug=True)
